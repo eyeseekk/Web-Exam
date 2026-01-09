0d028889717fafdfde1124d2e2018868bf379716
Ais <karsaev16@mail.ru>
2026-01-09 15:21:47 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
  * Глобальные константы конфигурации
  */
 const API_KEY = 'cb5b3b0c-7518-4730-86c3-79454ef67a44';
-const BASE_URL = 'http://exam-api-courses.std-900.ist.mospolytech.ru/api';
-// const BASE_URL = 'https://corsproxy.io/?' + 
-//   encodeURIComponent('http://exam-api-courses.std-900.ist.mospolytech.ru/api');
+// const BASE_URL = 'http://exam-api-courses.std-900.ist.mospolytech.ru/api';
+const BASE_URL = 'https://corsproxy.io/?' + 
+  encodeURIComponent('http://exam-api-courses.std-900.ist.mospolytech.ru/api');
 
 /**
  * Вспомогательные функции
